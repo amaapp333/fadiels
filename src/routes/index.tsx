6c30b88ea84e156b7b1e7073548cab70a0e57f93
amaapp333 <118077998+amaapp333@users.noreply.github.com>
2022-11-27 06:24:59 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,12 +4,12 @@ import "./index.css";
 export default function Home() {
   return (
     <main>
-      <h1>Steak News!</h1>
+      <h1>Fadiel's News!</h1>
       <Counter />
       <p>
         Visit{" "}
-        <a href="https://fadiels.vercel.app" target="_blank">
-          Fadiel.Vercel.App
+        <a href="https://fadiels.vercel.app/privacy" target="_blank">
+          fadiel.vercel
         </a>{" "}
         to learn how to build Solid apps.
       </p>
